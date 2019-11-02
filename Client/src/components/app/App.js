2014da16e669748737a290f53740d1e1a796c6da
Saul Ibarra <saulibarra@MacBook-Pro-de-Saul.local>
2019-11-02 11:42:31 -0500

    add all the informative components

--- Client/src/components/app/App.js
+++ Client/src/components/app/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "../homePage/HomePage";
 import SignUp from "../signUp/SignUp";
+import Aduana from "../aduana/Aduana";
+import Mural from "../mural/Mural";
+import Museo from "../museo/Museo";
+
 import * as ROUTES from "../../routes/Routes";
 
 import Navigation from "../navigation/Navigation";
@@ -15,6 +19,9 @@ function App() {
         <Switch>
           <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
           <Route exact path={ROUTES.HOME} component={HomePage} />
+          <Route exact path={ROUTES.ADUANA} component={Aduana} />
+          <Route exact path={ROUTES.MURAL} component={Mural} />
+          <Route exact path={ROUTES.MUSEO} component={Museo} />
         </Switch>
       </div>
     </Router>
--- Client/src/routes/Routes.js
+++ Client/src/routes/Routes.js
@@ -1,3 +1,5 @@
-
 export const SIGN_UP = "/signup";
 export const HOME = "/";
+export const ADUANA = "/aduana";
+export const MURAL = "/mural";
+export const MUSEO = "/museo";
