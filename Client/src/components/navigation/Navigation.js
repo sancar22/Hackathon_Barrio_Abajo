6a064169129ea9e25c08b9619432050fc8f32fc3
unknown <santiagoyacaman@uninorte.edu.co>
2019-11-02 01:56:21 -0500

    Joined_navbar_with_sign_in_up

--- Client/src/components/navigation/Navigation.js
+++ Client/src/components/navigation/Navigation.js
@@ -7,39 +7,10 @@ import { useSelector, useDispatch } from "react-redux";
 import {selectButton} from '../../actions'
 
 function Navigation(props) {
-<<<<<<< HEAD
-const dispatch = useDispatch();
-
- const click = () => {
+  const dispatch = useDispatch()
+  const click = () =>{
       dispatch(selectButton())
- }
-  return (
-    <body className="bodyy">
-      <section>
-        <header>
-          <div className="navBox">
-            <ul>
-              <a>
-                <li>
-                  <Link to={ROUTES.HOME}>Home</Link>
-                </li>
-              </a>
-              <a>
-                <li>
-                  <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-                </li>
-              </a>
-              
-            </ul>
-         
-          <button onClick ={click}>
-            Click
-          </button>
-          </div>
-        </header>
-      </section>
-    </body>
-=======
+  }
   return (
     <nav className="navbar navbar-expand-lg pt-0 pb-0 ">
       <h1 className="navbar-brand brand">Nombre de la empresa</h1>
@@ -58,11 +29,10 @@ const dispatch = useDispatch();
       <div className="collapse navbar-collapse " id="navbarSupportedContent">
         {/* align-items-center */}
         <ul className="but navbar-nav mr-5 ml-auto justify-content-end  ">
-          <button className="btn btn-primary align-middle">Ingresar</button>
+          <button onClick= {click}className="btn btn-primary align-middle">Ingresar</button>
         </ul>
       </div>
     </nav>
->>>>>>> 5dd129656a78b10d7c2a556203f195d434198ab1
   );
 }
 export default withRouter(Navigation);
