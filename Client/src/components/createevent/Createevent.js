283bddd3d3e3c03ff8042b40d19ae21e8dda52b7
Saul Ibarra <saulibarra@MacBook-Pro-de-Saul.local>
2019-11-03 14:01:32 -0500

    event form

--- Client/src/components/createevent/Createevent.js
+++ Client/src/components/createevent/Createevent.js
@@ -8,12 +8,12 @@ import {
   closeButtonSP,
   openEventsCreator
 } from "../../actions/index";
-import "./Comments.css";
+import "./Createevent.css";
 
 function Createevent(props) {
   return (
-    <div className="eventContainer">
-      <div className="loginBox">
+    <div className="eventContainer container">
+      <div className="loginBoxe">
         <div className="row">
           <div className="col-md-12">
             <h3 onClick="" className="parax float-right">
@@ -22,33 +22,78 @@ function Createevent(props) {
           </div>
         </div>
         <h2>¡Muestrale al mundo tus iniciativas!</h2>
-        <div className="formul">
+        <div className="formul1">
           <form onSubmit="">
-            <p>Titulo</p>
-            <input type="text" placeholder="Titulo" value="" onChange="" />
-            <p>Descripción</p>
-            <div style={{ display: "inline" }}>
-              <input type="textarea" placeholder="" value="" onChange="" />
-              <input type="radio" name="gender" value="social" />
-            </div>
-            Social
-            <br />
-            <input type="radio" name="gender" value="cultural" />
-            Cultural
-            <input type="radio" name="gender" value="aseo" />
-            Aseo
-            <input type="radio" name="gender" value="deportivo" />
-            Deportivo
+            <p className="tis">Titulo</p>
             <input
-              type="submit"
-              style={{ margginTop: "8vh" }}
-              name=""
-              value="Sign In"
+              type="text"
+              className="ins"
+              placeholder="Titulo"
+              value=""
+              onChange=""
             />
+            <p className="tis">Descripción</p>
+            <div style={{ display: "inline" }}>
+              <input
+                type="textarea"
+                placeholder=""
+                className="ins"
+                value=""
+                onChange=""
+              />
+            </div>
+            <p className="tis">Lugar</p>
+            <input
+              type="text"
+              className="ins"
+              placeholder="Lugar"
+              value=""
+              onChange=""
+            />
+            <p className="tis">Telefono de contacto</p>
+            <input
+              type="string"
+              className="ins"
+              placeholder="Telefono"
+              value=""
+              onChange=""
+            />
+            <div className="dropp">
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle col-md-12"
+                  type="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Seleccionar ...
+                </button>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton"
+                >
+                  <a className="dropdown-item" href="#">
+                    Cultural
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Deportivo
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Social
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Aseo
+                  </a>
+                </div>
+              </div>
+            </div>
+            <p className="tis">Fecha</p>
+            <input type="date" className="ins" style={{ margginTop: "4vh" }} />
+            <div className="fin">
+              <input type="submit" className="butt btn mt2 col-md-12" />
+            </div>
           </form>
-          <a className="acomp" onClick="">
-            ¿No tienes cuenta aún? Regístrate.
-          </a>
         </div>
       </div>
     </div>
