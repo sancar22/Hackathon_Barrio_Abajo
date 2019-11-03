0005f331526f75e2bbe8d8448e7b2b28a631246e
Saul Ibarra <saulibarra@MacBook-Pro-de-Saul.local>
2019-11-03 07:14:52 -0500

    new welcome page

--- Client/src/components/intro/Intro.js
+++ Client/src/components/intro/Intro.js
@@ -28,14 +28,38 @@ function Intro(props) {
         className="divH visible"
         style={{ opacity: displayHome ? 0 : 1, display: displayVisible }}
       >
-        <img src="cofre gofre.png" alt="" style={{ width: "100vw", height: "100vh" }}/>
+        <img
+          src="cofre gofre.png"
+          alt=""
+          style={{ width: "100vw", height: "100vh" }}
+        />
         <button className="buttonD" onClick={dispHome}>
           Descubre
         </button>
       </div>
-      <button className="butti" onClick={handleClick}>
-        Mapa
-      </button>
+      <div className="backscreen"></div>
+      <div className="darker "></div>
+      <div className="content container">
+        <div className="row">
+          <div className="col-md-1"></div>
+          <h1 className=" welcome text-white">¡BIENVENIDO!</h1>
+        </div>
+        <div className="row">
+          <p className="text-light introduc col-md-6">
+            Barrio Abajo es uno de los primeros barrios de Barranquilla y cuenta
+            con una gran historia e importancia pora la ciudad y el pais. Asi,
+            nuestra plataforma ha sido creada con el fin de redescubrir el
+            Barrio Abajo, conocer sobre sus necesidades y aportar para su
+            desarrollo; para el desarrollo de nuestro Tesoro Quillero.
+          </p>
+        </div>
+        <button
+          className="row butti btn-secondary btn-lg "
+          onClick={handleClick}
+        >
+          Vamos a descubrirlo
+        </button>
+      </div>
     </div>
   );
 }
