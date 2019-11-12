59c92b6dda85856ae4c91d6065c601a67502591a
unknown <santiagoyacaman@uninorte.edu.co>
2019-11-11 19:47:49 -0500

    Updates

---
+++ precache-manifest.69ae76faed8f2b8aa97c780ba858424b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f05e7e9b38a44fe82b0bc2cac13d44",
+    "url": "/Hackathon_Barrio_Abajo/index.html"
+  },
+  {
+    "revision": "4b8b1e9d530757d32083",
+    "url": "/Hackathon_Barrio_Abajo/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "bc7f640d72cf8ae85cea",
+    "url": "/Hackathon_Barrio_Abajo/static/css/main.e882433a.chunk.css"
+  },
+  {
+    "revision": "4b8b1e9d530757d32083",
+    "url": "/Hackathon_Barrio_Abajo/static/js/2.9b4642cd.chunk.js"
+  },
+  {
+    "revision": "bc7f640d72cf8ae85cea",
+    "url": "/Hackathon_Barrio_Abajo/static/js/main.efcff59e.chunk.js"
+  },
+  {
+    "revision": "7389d15f5bd312058bc3",
+    "url": "/Hackathon_Barrio_Abajo/static/js/runtime~main.bfbb7410.js"
+  },
+  {
+    "revision": "44926d81e94482fa3637c17098d92f42",
+    "url": "/Hackathon_Barrio_Abajo/static/media/LOGO.44926d81.png"
+  },
+  {
+    "revision": "489024cd66b2c14f77ca4ee8f34065bb",
+    "url": "/Hackathon_Barrio_Abajo/static/media/Mural.489024cd.png"
+  },
+  {
+    "revision": "e374732a133e4afa4d061c27a3a5e579",
+    "url": "/Hackathon_Barrio_Abajo/static/media/aduana.e374732a.png"
+  },
+  {
+    "revision": "5171a3f87e3c7c1ca47a804982bc3892",
+    "url": "/Hackathon_Barrio_Abajo/static/media/carnaval.5171a3f8.png"
+  },
+  {
+    "revision": "7778352f9e8314a226cd617ef85b39de",
+    "url": "/Hackathon_Barrio_Abajo/static/media/casa.7778352f.png"
+  },
+  {
+    "revision": "9b0588fe561aa7266dd067833c002c1c",
+    "url": "/Hackathon_Barrio_Abajo/static/media/casas.9b0588fe.png"
+  },
+  {
+    "revision": "4f910dad6c872e6b12705e6099a79023",
+    "url": "/Hackathon_Barrio_Abajo/static/media/mmm.4f910dad.png"
+  },
+  {
+    "revision": "3ef42adac1dd2dee1bcbfa042d17512d",
+    "url": "/Hackathon_Barrio_Abajo/static/media/museo.3ef42ada.png"
+  }
+]);
